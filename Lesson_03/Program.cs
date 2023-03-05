1ac5c5cd7cc7bcb5b085063c60cbd864032cb85e
MukolkaLengyel <moonlight312004@gmail.com>
2023-03-05 21:40:17 +0100

    C# code from Lesson_03

---
+++ Lesson_03/Program.cs
@@ -0,0 +1,21 @@
+﻿//first problem
+using System.Collections.Generic;
+for (int i = 10; i<13; ++i)
+{
+  Console.WriteLine("The First sollution is:");
+  Console.WriteLine(i++ + i++ +i);
+}
+
+//second problem
+for (int j = 2; j<=5; j++)
+{
+    Console.WriteLine("The Second sollution is: ");
+    Console.WriteLine(j++ + j++ + j++ + j++);
+}
+
+//third problem
+for (int a = 10; a == 10; a++)
+{
+    Console.WriteLine("The Third sollution is: ");
+    Console.WriteLine(a);
+}
